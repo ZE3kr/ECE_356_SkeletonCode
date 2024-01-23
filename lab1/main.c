99a4904ad96d702a8f33318c5377aba680a3d95a
James Guo <i@ze3kr.com>
2024-01-23 18:20:03 -0500

    Lab1

---
+++ lab1/main.c
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>302 Found</title>
+</head><body>
+<h1>Found</h1>
+<p>The document has moved <a href="https://courses.cs.duke.edu//spring18/compsci356/labs/lab1/main.c">here</a>.</p>
+<hr>
+<address>Apache/2.4.41 (Ubuntu) Server at www2.cs.duke.edu Port 443</address>
+</body></html>
